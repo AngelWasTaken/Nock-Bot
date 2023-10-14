5aebb643f38d77f87850d4f95dfe561c995d89ad
Angle <113272597+AngelWasTaken@users.noreply.github.com>
2023-10-14 02:11:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-import bot
+from discord.ext import commands
+import discord
 
-if __name__ == '__main__':
-    bot.run_discord_bot()
-##HOW ARE YOU OUT OF DATE??????
+BOT_TOKEN = "MTA1NDQ0MzcxNzc2MDY0NzE2OQ.GGioCP.nvMZRdYlON_wYpSfNAis3xX-vBcHSlW5oeIimI"
+CHANNEL_ID = 1041579268305584190
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Howdy :cowboy:")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("Howdy :cowboy:")
+#on startup
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+#("!Hello") = ("Hello!")
+@bot.command()
+async def test(ctx):
+    await ctx.send("https://youtu.be/ykwqXuMPsoc?si=MndZ5AVvHA3H9srM")
+#This is a youtube song leading to narwals in the ocean
+bot.run(BOT_TOKEN)
